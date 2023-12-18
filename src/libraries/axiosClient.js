13bfa22d925498248913a91d539085ad43ec98c8
Mr.Thang <vothang226@gmail.com>
2023-12-18 18:19:12 +0700

    up

--- src/libraries/axiosClient.js
+++ src/libraries/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000",
-  // baseURL: "https://ecommerce-user-be.onrender.com",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://ecommerce-user-be.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
