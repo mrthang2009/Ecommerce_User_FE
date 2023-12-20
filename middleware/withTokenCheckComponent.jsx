f052914e13c0a9c49aa79af724842cf2efd4110b
Mr.Thang <vothang226@gmail.com>
2023-12-20 14:18:02 +0700

    up

--- middleware/withTokenCheckComponent.jsx
+++ middleware/withTokenCheckComponent.jsx
@@ -1,18 +1,18 @@
-import axiosClient from '@/libraries/axiosClient';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { toast } from 'react-toastify';
+import axiosClient from "@/libraries/axiosClient";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const withTokenCheckComponent = (WrappedComponent, redirectPage) => {
   return (props) => {
     const router = useRouter();
- // Lấy token từ local storage hoặc nơi lưu trữ khác
-    
+    // Lấy token từ local storage hoặc nơi lưu trữ khác
+
     useEffect(() => {
       const token = localStorage.getItem("TOKEN");
       // Kiểm tra xem token có tồn tại trong localStorage hay không
       axiosClient.defaults.headers.Authorization = `Bearer ${token}`;
-
+      console.log("««««« vào đây »»»»»");
       if (!token) {
         if (redirectPage) {
           router.push(redirectPage); // Chuyển hướng đến trang login hoặc trang khác tùy chọn
