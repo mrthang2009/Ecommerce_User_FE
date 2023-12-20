f6c1527dce07a587aa580544ced81cc9e2adcea7
Mr.Thang <vothang226@gmail.com>
2023-12-20 18:30:25 +0700

    haha

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* font Dong le */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Dongle:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/logo.jpg" />
       </Head>
       <body>
         <Main />
