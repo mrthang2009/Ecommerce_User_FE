d675eebd5eee01df2babb1e3608767da0d6d6e86
Mr.Thang <vothang226@gmail.com>
2023-12-20 19:51:51 +0700

    add

--- middleware/withTokenCheckComponent.jsx
+++ middleware/withTokenCheckComponent.jsx
@@ -9,46 +9,46 @@ const withTokenCheckComponent = (WrappedComponent, redirectPage) => {
     const router = useRouter();
 
     useEffect(() => {
-    const checkAndRefreshToken = async () => {
-      let token = localStorage.getItem("TOKEN");
-      let refreshToken = localStorage.getItem("REFRESH_TOKEN");
+      const checkAndRefreshToken = async () => {
+        let token = localStorage.getItem("TOKEN");
+        let refreshToken = localStorage.getItem("REFRESH_TOKEN");
 
-      if (!token) {
-        if (redirectPage) {
-          router.push(redirectPage);
-        } else {
-          router.push("/login");
-        }
-      } else {
-        const decodedPayloadToken = decodeToken(token);
-        const decodedPayloadRefreshToken = decodeToken(refreshToken);
-        if (decodedPayloadRefreshToken.exp >= Date.now() / 1000) {
-          if (decodedPayloadToken.exp < Date.now() / 1000) {
-            try {
-              const res = await axiosClient.post(`auth/refesh-token`, {
-                refreshToken,
-              });
-              const newToken = res.data.token;
-              localStorage.setItem("TOKEN", newToken);
-              axiosClient.defaults.headers.Authorization = `Bearer ${newToken}`;
-            } catch (error) {
-              console.error("Error refreshing token:", error);
-            }
-          } else {
-            axiosClient.defaults.headers.Authorization = `Bearer ${token}`;
-          }
-        } else {
+        if (!token) {
           if (redirectPage) {
             router.push(redirectPage);
           } else {
             router.push("/login");
           }
-          localStorage.removeItem("TOKEN");
-          localStorage.removeItem("REFRESH_TOKEN");
-          toast.error("Phiên đăng nhập hết hạng vui lòng đăng nhập lại");
+        } else {
+          const decodedPayloadToken = decodeToken(token);
+          const decodedPayloadRefreshToken = decodeToken(refreshToken);
+          if (decodedPayloadToken.exp < Date.now() / 1000) {
+            if (decodedPayloadRefreshToken.exp >= Date.now() / 1000) {
+              try {
+                const res = await axiosClient.post(`auth/refesh-token`, {
+                  refreshToken,
+                });
+                const newToken = res.data.token;
+                localStorage.setItem("TOKEN", newToken);
+                axiosClient.defaults.headers.Authorization = `Bearer ${newToken}`;
+              } catch (error) {
+                console.error("Error refreshing token:", error);
+              }
+            } else {
+              if (redirectPage) {
+                router.push(redirectPage);
+              } else {
+                router.push("/login");
+              }
+              localStorage.removeItem("TOKEN");
+              localStorage.removeItem("REFRESH_TOKEN");
+              toast.error("Phiên đăng nhập hết hạng vui lòng đăng nhập lại");
+            }
+          } else {
+            axiosClient.defaults.headers.Authorization = `Bearer ${token}`;
+          }
         }
-      }
-    };
+      };
 
       checkAndRefreshToken();
     }, [redirectPage]);
